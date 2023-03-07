fe2f0787d4324e1670618b9d561b778975e0c54b
tarasmoroz <taras.moroz@ekreative.com>
2023-03-07 15:00:53 +0100

    Added fixtures for lot users and mobile numbers

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,11 +6,15 @@ use App\Entity\MobileNumber;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        //Use faker
+        $faker = Factory::create('EN_en');
+
         //Fixtures for one user
         $user = new User();
         $user
@@ -40,7 +44,7 @@ class UserFixtures extends Fixture
 
         $mobileNumberTwo = new MobileNumber();
         $mobileNumberTwo
-            ->setNameOperator('Vodofone')
+            ->setNameOperator('Vodafone')
             ->setCodeCountry('380')
             ->setCodeOperator('50')
             ->setNumber('7654321')
@@ -64,6 +68,41 @@ class UserFixtures extends Fixture
         ;
 
         $manager->persist($mobileNumberThree);
+
+        //Fixtures for lot users and mobile numbers
+        for ($i=1; $i<=4; $i++)
+        {
+            $user = new User();
+            $user
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setBirthday($faker->dateTimeThisYear)
+            ;
+            $manager->persist($user);
+
+            $countMobileNumbers = rand(1,3);
+
+            for ($j=1; $j<=$countMobileNumbers; $j++)
+            {
+                $randNumber = rand(0,3);
+                $operators = ['Vodafone','KyivStar','Life','KyivStar'];
+                $codeOperators = [50, 67, 63, 68];
+                $number = rand(1000000,9999999);
+                $balance = rand(-150.0,150.0);
+
+                $mobileNumber = new MobileNumber();
+                $mobileNumber
+                    ->setNameOperator($operators[$randNumber])
+                    ->setCodeCountry('380')
+                    ->setCodeOperator($codeOperators[$randNumber])
+                    ->setNumber($number)
+                    ->setBalance((float) $balance)
+                    ->setUser($user)
+                ;
+
+                $manager->persist($mobileNumber);
+            }
+        }
 
         $manager->flush();
     }
