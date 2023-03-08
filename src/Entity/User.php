fb67760a7b74ff874bc4b287955bff8a2c4928aa
tarasmoroz <taras.moroz@ekreative.com>
2023-03-08 20:57:34 +0100

    removed not used class

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,9 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use JetBrains\PhpStorm\ArrayShape;
 use ReturnTypeWillChange;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
